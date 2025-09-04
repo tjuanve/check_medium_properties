51e864f38435b952caf50ba8b6a8261fbabcdec7
Thijs van Eeden <tvaneede@cobalt-10.icecube.wisc.edu>
2025-09-04 04:18:19 -0500

    first commit

---
+++ setenv.sh
@@ -0,0 +1,4 @@
+
+/cvmfs/icecube.opensciencegrid.org/py3-v4.4.1/RHEL_7_x86_64_v2/metaprojects/icetray/v1.14.0/bin/icetray-shell
+
+source /data/user/tvaneede/software/py_venvs/py3-v4.4.1_reco-v1.1.0/bin/activate
